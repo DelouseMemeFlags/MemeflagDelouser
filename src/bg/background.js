153e12da783adb2f5d8e0db18c19ff1f07230ae7
BloomTech14 <cohoz@mailing.one>
2019-04-12 13:32:15 +0100

    remove unused bg files

--- src/bg/background.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (typeof chrome.pageAction !== 'undefined') {
-        chrome.pageAction.show(sender.tab.id);
-      }
-      sendResponse();
-    });
